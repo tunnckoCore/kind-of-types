25050a8af6c2257e6a3140df42c651df1553d59c
tunnckoCore <mameto_100@mail.bg>
2015-07-04 02:10:49 +0300

    implement :star2:

---
+++ index.js
@@ -0,0 +1,10 @@
+/*!
+ * kind-of-types <https://github.com/tunnckoCore/kind-of-types>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = require('./list.json')
---
+++ test.js
@@ -0,0 +1,17 @@
+/*!
+ * kind-of-types <https://github.com/tunnckoCore/kind-of-types>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+/* jshint asi:true */
+
+'use strict'
+
+// var test = require('assertit')
+// var kindOfTypes = require('./index')
+
+// test('kind-of-types:', function () {
+//   // body
+// })
